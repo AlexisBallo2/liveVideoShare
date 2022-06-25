3f95c5bfd5a320148221a672c27b32ede2a89ba2
Alexis Ballo <alexispballo@gmail.com>
2022-06-25 00:37:01 -0400

    refactor + working with simple timer

--- client/App.js
+++ client/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import socketClient  from "socket.io-client";
+import React from "react";
+import socketClient from "socket.io-client";
 const SERVER = "http://127.0.0.1:8080";
 function App() {
-    var socket = socketClient (SERVER);
-	socket.on('connection', 90 => {console.log("connected with backend")})
-    return (
-        <div classname="App">
-
-	</div>
-	)
+        const socket = socketClient(SERVER);
+        socket.on("connection", () => {
+                console.log("connected with backend");
+        });
+        return <div className="App"></div>;
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,40 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const PORT = 8080;
-var cors = require('cors');
-var io = require('socket.io')(server, {cors: {origin: '*'}});
+var cors = require("cors");
+var io = require("socket.io")(server, { cors: { origin: "*" } });
 
 server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
+  console.log(`listening on *:${PORT}`);
 });
 
-curTime = 0; 
+curTime = 0;
 pause = false;
-setInterval(function(){
-	if(pause === false) {	
-		curTime+=1;
-	}
-	},1000);
-io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
-    //console.log('new client connected');
-	socket.emit('connection',null);
-	setInterval(function(){
-		socket.emit('timestamp', curTime);
-	}, 1000);
-	socket.on('pause', (time) => {
-	//	curTime = time;
-		pause = true;
-		socket.emit('pause',true);
-	})
-	socket.on('unpause',() => {
-		pause = false;
-		socket.emit('pause', false);
-	})
 
+setInterval(function () {
+  if (pause === false) {
+    curTime += 1;
+  }
+}, 1000);
 
+io.on("connection", (socket) => {
+  /* socket object may be used to send specific messages to the new connected client */
+  //console.log('new client connected');
+
+ // socket.emit("timestamp", curTime);
+
+  setInterval(function () {
+    socket.emit("timestamp", curTime);
+  }, 1000);
+
+  //on recieving a pause from the client
+  socket.on("pause", (recieved) => {
+    curTime = recieved.time;
+    pause = recieved.bool;
+    //emit the currenttime and paused state
+    socket.emit("pause", { bool: pause, time: curTime });
+  });
 });
-
-
-
